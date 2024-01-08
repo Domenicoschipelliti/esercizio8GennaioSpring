17eafd2a1aaec8a84c303aa474fcdca72b74b5f6
Domenicoschipelliti <142498764+Domenicoschipelliti@users.noreply.github.com>
2024-01-08 13:46:44 +0100

    inizio

---
+++ src/main/java/Domenico/Pizza8Gennaio/Pizza8GennaioApplication.java
@@ -0,0 +1,13 @@
+package Domenico.Pizza8Gennaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pizza8GennaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pizza8GennaioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Domenico/Pizza8Gennaio/Pizza8GennaioApplicationTests.java
@@ -0,0 +1,13 @@
+package Domenico.Pizza8Gennaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pizza8GennaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
